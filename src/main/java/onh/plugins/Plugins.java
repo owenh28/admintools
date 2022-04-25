20152426e677073d8c6bd697baa5c9f6852cda1f
owenh <owenharlacker@outlook.com>
2022-04-25 12:31:54 -0600

    First commit

---
+++ src/main/java/onh/plugins/Plugins.java
@@ -0,0 +1,20 @@
+package onh.plugins;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Plugins extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getLogger().info("GOOD MORNING VIETNAM!!!!");
+        getLogger().info("Owen's AdminTools has loaded successfully!");
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
