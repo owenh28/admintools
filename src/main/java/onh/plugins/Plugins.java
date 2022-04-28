7993a409108d306314abbe0f0388ddab99ab0abd
owenh <owenharlacker@outlook.com>
2022-04-28 15:10:18 -0600

    mo bitches

--- src/main/java/onh/plugins/Plugins.java
+++ src/main/java/onh/plugins/Plugins.java
@@ -1,5 +1,6 @@
 package onh.plugins;
 
+import io.papermc.paper.event.player.PlayerDeepSleepEvent;
 import onh.plugins.commands.beacon;
 import onh.plugins.commands.beaconother;
 import onh.plugins.commands.bitches;
@@ -9,6 +10,7 @@ import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.EventListener;
 import java.util.Objects;
 
 public final class Plugins extends JavaPlugin {
@@ -19,19 +21,24 @@ public final class Plugins extends JavaPlugin {
         getLogger().info("GOOD MORNING VIETNAM!!!!");
         getLogger().info("Owen's AdminTools has loaded successfully!");
         //Register command executors for all commands
-        this.getCommand("adminfo").setExecutor(new info());
-        this.getCommand("beacon").setExecutor(new beacon());
-        this.getCommand("beaconother").setExecutor(new beaconother());
-        this.getCommand("bitches").setExecutor(new bitches());
-        //Single player sleep?
+        Objects.requireNonNull(this.getCommand("adminfo")).setExecutor(new info());
+        Objects.requireNonNull(this.getCommand("beacon")).setExecutor(new beacon());
+        Objects.requireNonNull(this.getCommand("beaconother")).setExecutor(new beaconother());
+        Objects.requireNonNull(this.getCommand("bitches")).setExecutor(new bitches());
 
-//        Server srv = getServer();
-//        World wld = srv.getWorld("world");
-//        Player[] onlp = srv.getOnlinePlayers().toArray(new Player[0]);
-//        for(int i = 0; i < srv.getOnlinePlayers().size(); i++){
-//            if(onlp[i].isSleeping()){
-//                assert wld != null;
-//                wld.setTime(6000);
+
+
+        //Single player sleep?
+//        while(true) {
+//
+//            Server srv = getServer();
+//
+//            Player[] onlp = srv.getOnlinePlayers().toArray(new Player[0]);
+//            for(int i = 0; i < srv.getOnlinePlayers().size(); i++){
+//                if(onlp[i].isSleeping()){
+//                    World wld = onlp[i].getWorld();
+//                    wld.setTime(6000);
+//                }
 //            }
 //        }
 
@@ -43,4 +50,5 @@ public final class Plugins extends JavaPlugin {
         // Plugin shutdown logic
         getLogger().info("Goodbye, see everyone next time...");
     }
+
 }
--- src/main/java/onh/plugins/commands/beacon.java
+++ src/main/java/onh/plugins/commands/beacon.java
@@ -10,22 +10,19 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.meta.FireworkMeta;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collections;
-
 public class beacon implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         //Make new player from the command sender
         Player pSend = (Player) sender;
-
-            //Build a firework from the player position and some preset values
-            //maybe add a firework customizer in the future
+        //Build a firework from the player position and some preset values
+        //maybe add a firework customizer in the future
         Firework fw = pSend.getWorld().spawn(pSend.getLocation(), Firework.class);
         FireworkMeta fwm = fw.getFireworkMeta();
         FireworkEffect effect = FireworkEffect.builder().withColor(Color.AQUA).with(FireworkEffect.Type.CREEPER).withFade(Color.FUCHSIA).withFlicker().build();
         fwm.addEffects(effect);
         fwm.setPower(1);
-            //launch the firework
+        //launch the firework
         fw.setFireworkMeta(fwm);
         pSend.sendMessage("You have been revealed!");
         return true;
