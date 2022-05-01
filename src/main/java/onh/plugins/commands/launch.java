a16a5212d497006a645cfdfb10b80fd5a80198c9
owenh <owenharlacker@outlook.com>
2022-05-01 02:08:41 -0600

    more collin bits

--- src/main/java/onh/plugins/commands/launch.java
+++ src/main/java/onh/plugins/commands/launch.java
@@ -1,13 +1,19 @@
 package onh.plugins.commands;
 
+import org.bukkit.Location;
 import org.bukkit.Server;
 import org.bukkit.block.Block;
+import org.bukkit.block.Structure;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.TNTPrimed;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class launch implements CommandExecutor {
 
 
@@ -19,6 +25,21 @@ public class launch implements CommandExecutor {
         }
         //get the player
         Player sndPl = (Player) sender;
+        Location plLoc = sndPl.getLocation();
+        List<Block> blockList = new ArrayList<>();
+
+        // CHnaged to  comment for github push
+ //       Location plLoc = sndPl.getLocation();
+
+
+        for(int i = 0; i < 6; i++){
+            int under = plLoc.getBlockX();
+            Block block = plLoc.getBlock();
+
+        }
+
+
+
         sndPl.setInvisible(true);
         sndPl.setInvulnerable(true);
         //declare a block that is 9 blocks away from what the player is looking.
@@ -28,6 +49,7 @@ public class launch implements CommandExecutor {
         TNTPrimed ttt = sndPl.getWorld().spawn(block.getLocation().add(sndPl.getLocation().getX(), sndPl.getLocation().getY(), sndPl.getLocation().getZ()), TNTPrimed.class );
 
         ttt.setFuseTicks(0);
+
 
         sndPl.setInvisible(false);
         sndPl.setInvulnerable(false);
